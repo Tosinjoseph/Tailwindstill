9011492df6fdb6ec6a4379b58107d42d5020f706
TosinJoseph <tosinjosephoffice2@gmail.com>
2025-03-10 15:03:29 -0700

    made changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,14 @@ const Navbar = () => {
   const[showMobileMenu, setShowMobileMenu] = useState(false)
 
   useEffect(()=>{
-    
-  },[])
+    if(showMobileMenu){
+      document.body.style.overflow= 'hidden'
+    }else{
+      document.body.style.overflow= 'auto'
+    }return()=>{
+      document.body.style.overflow= 'auto'
+    }
+  },[showMobileMenu])
   return (
   <div className='top-0 left-0  right-0 absolute z-10 w-full '>
     <div className=' right-0 w-full px-6 md:px-20 lg:px-28 py-4
@@ -22,15 +28,15 @@ const Navbar = () => {
         <img onClick={()=>setShowMobileMenu(true)} src="./menu_icon.svg" className='z-25 cursor-pointer w-8  md:hidden filter invert' alt="" />  
     </div>
     <div className={`fixed ${showMobileMenu? ' w-full' : 'hidden '} md:hidden top-0 bottom-0 left-0 bg-white  `}>
-    <div className='flex justify-end p-4'>
+    <div className='flex justify-end p-7'>
     <img onClick={()=>setShowMobileMenu(false)} src="./cross_icon.svg" className='z-27 hover:scale-110 cursor-pointer w-8 md:hidden ' alt="" />
     </div>
       <ul className='z-25 flex flex-col w-full text-center  '>
-      <a className="hover:text-blue-700 mb-2 hover:font-bold text-sm" href='#Home' >HOME</a>
-          <a className="hover:text-blue-700 mb-2 hover:font-bold  text-sm" href='#Feature' >FEATURES</a>
-          <a className="hover:text-blue-700 mb-2 hover:font-bold  text-sm" href='#Pricing' >PRICING</a>
-          <a className="hover:text-blue-700 mb-2 hover:font-bold text-sm" href='#Download' >DOWNLOAD</a>
-          <a className="hover:text-blue-700 mb-2 hover:font-bold text-sm" href='#Contact'>CONTACT</a> 
+          <a onClick={()=>setShowMobileMenu(false)} className="hover:text-blue-700 mb-2 hover:font-bold text-sm" href='#Home' >HOME</a>
+          <a onClick={()=>setShowMobileMenu(false)} className="hover:text-blue-700 mb-2 hover:font-bold  text-sm" href='#Feature' >FEATURES</a>
+          <a onClick={()=>setShowMobileMenu(false)} className="hover:text-blue-700 mb-2 hover:font-bold  text-sm" href='#Pricing' >PRICING</a>
+          <a onClick={()=>setShowMobileMenu(false)} className="hover:text-blue-700 mb-2 hover:font-bold text-sm" href='#Download' >DOWNLOAD</a>
+          <a onClick={()=>setShowMobileMenu(false)} className="hover:text-blue-700 mb-2 hover:font-bold text-sm" href='#Contact'>CONTACT</a> 
       </ul>
     
       </div>
